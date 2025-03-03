e61efa7120c24a6459ea5e1a8882003929035111
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2025-03-03 22:51:06 +0100

    chore: configure environment

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
+import { FlatCompat } from "@eslint/eslintrc";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +10,25 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript", "prettier"],
+    plugins: ["simple-import-sort"],
+    rules: {
+      "simple-import-sort/imports": [
+        "error",
+        {
+          groups: [["^\\u0000"], ["^@?\\w"], ["^[^.]"], ["^\\."]],
+        },
+      ],
+      "simple-import-sort/exports": "error",
+      "@typescript-eslint/consistent-type-imports": "error",
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      ],
+    },
+  }),
 ];
 
 export default eslintConfig;
---
+++ prettier.config.mjs
@@ -0,0 +1,10 @@
+﻿const prettierConfig = {
+  semi: true,
+  trailingComma: "all",
+  printWidth: 80,
+  tabWidth: 2,
+  bracketSameLine: true,
+  endOfLine: "crlf",
+};
+
+export default prettierConfig;
