6bac29f7f7da52424e2e666273b9c646a04b81fa
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2025-03-05 15:02:10 +0100

    chore: install and configure axios client

---
+++ src/lib/axios/axios-client.ts
@@ -0,0 +1,15 @@
+﻿import axios from "axios";
+
+import { requestInterceptors } from "@/lib/axios/request-interceptors";
+
+const axiosClient = axios.create({
+  baseURL: process.env.REST_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+});
+
+axiosClient.interceptors.request.use(requestInterceptors);
+
+export default axiosClient;
