3e47fb60957e60b05b5db2037d04838fdef51dba
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2025-03-09 17:17:00 +0100

    feat: implement collapsible element for filters, change chips backgrounds

--- src/components/filters/filter-section.tsx
+++ src/components/filters/filter-section.tsx
@@ -1,14 +1,35 @@
-﻿type FilterSectionProps = {
+﻿import { LucideChevronsUpDown } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible";
+
+type FilterSectionProps = {
   title: string;
   children: React.ReactNode;
 };
 
 export const FilterSection = ({ title, children }: FilterSectionProps) => {
   return (
-    <div>
-      <h2 className="text-xl font-bold mb-3">{title}</h2>
+    <Collapsible defaultOpen>
+      <div className="flex justify-between mb-3">
+        <h2 className="text-xl font-bold ">{title}</h2>
 
-      <div className="flex flex-wrap gap-2">{children}</div>
-    </div>
+        <CollapsibleTrigger asChild>
+          <Button variant="ghost" size="sm" className="w-9 p-0">
+            <LucideChevronsUpDown className="h-4 w-4" />
+
+            <span className="sr-only">Toggle</span>
+          </Button>
+        </CollapsibleTrigger>
+      </div>
+
+      <CollapsibleContent className="flex flex-wrap gap-2 overflow-hidden data-[state=open]:animate-collapsible-down data-[state=closed]:animate-collapsible-up">
+        {children}
+      </CollapsibleContent>
+    </Collapsible>
   );
 };
